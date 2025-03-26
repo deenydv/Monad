c9b9a4661a0e808ac708127257b5f4754680e4f7
Zaharadden mansur <ai.dao.dev@gmail.com>
2025-03-26 15:18:56 +0100

    Update GmonadNFT.sol

--- GmonadNFT.sol
+++ GmonadNFT.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GmonadNFT is ERC721, Ownable {
+    uint256 public nextTokenId;
+    uint256 public mintPrice = 0.01 ether;
+
+    constructor() ERC721("GmonadNFT", "GMNNFT") Ownable(msg.sender) {}
+
+    function mint() external payable {
+        require(msg.value >= mintPrice, "Not enough ETH to mint");
+        _safeMint(msg.sender, nextTokenId);
+        nextTokenId++;
+    }
+
+    function setMintPrice(uint256 _price) external onlyOwner {
+        mintPrice = _price;
+    }
+}
