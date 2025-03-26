f995e472aed2f2b72bf4e6fc68a94e0138e8c5ac
Zaharadden mansur <ai.dao.dev@gmail.com>
2025-03-26 15:21:37 +0100

    Update Gmonadairdrop.sol

--- Gmonadairdrop.sol
+++ Gmonadairdrop.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GmonadAirdrop is Ownable {
+    IERC20 public token;
+
+    constructor(address _token) Ownable(msg.sender) {
+        token = IERC20(_token);
+    }
+
+    function airdrop(address[] calldata recipients, uint256 amount) external onlyOwner {
+        for (uint256 i = 0; i < recipients.length; i++) {
+            require(token.transfer(recipients[i], amount), "Transfer failed");
+        }
+    }
+}
